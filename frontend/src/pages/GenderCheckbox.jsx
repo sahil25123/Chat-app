e3f96fdaef60e6ea6ccfd5b804864ce8694c0946
sahil25123 <sahilgupta25123@gmail.com>
2025-05-06 23:02:24 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/GenderCheckbox.jsx
+++ frontend/src/pages/GenderCheckbox.jsx
@@ -4,7 +4,7 @@ const GenderCheckbox = ({onCheckboxChange , selectedGender}) => {
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer`}>
 					<span className='label-text text-gray-600'>Male</span>
-					<input checked={selectedGender ==="male"}  onChange={()=> onCheckboxChange("male")} type='checkbox' className='checkbox border-slate-900' />
+					<input checked={selectedGender ==="male"}  onChange={()=> onCheckboxChange("male")} type='checkbox' className='checkbox checkbox-primary border-slate-900' />
 				</label>
 			</div>
 			<div className='form-control'>
@@ -12,7 +12,7 @@ const GenderCheckbox = ({onCheckboxChange , selectedGender}) => {
 					<span className='label-text text-gray-600 px-2'>Female</span>
 					<input  checked= {selectedGender ==="female"}
 						onChange = {()=>onCheckboxChange("female")}
-					type='checkbox' className='checkbox border-slate-900' />
+					type='checkbox' className='checkbox checkbox-primary border-slate-900' />
 				</label>
 			</div>
 		</div>
