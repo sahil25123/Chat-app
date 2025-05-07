af5475342f432c5e402b4868456bf931b91d9b27
sahil25123 <sahilgupta25123@gmail.com>
2025-05-07 12:42:28 +0530

    fix: correct static file serving and comment out catch-all route for SPA

--- backend/index.js
+++ backend/index.js
@@ -37,11 +37,12 @@ app.use("/api/message",msgRoutes);
 app.use("/api/users",userRoutes)
 
 
-app.use(express.static(path.join(__dirname , "/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname , "frontend" ,"dist" , "index.html"))
-})
+// // Catch-all route for SPA
+// app.get("/:path(*)", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(port,()=>{
     connectToMongoDB();
